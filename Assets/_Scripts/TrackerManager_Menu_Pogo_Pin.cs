20b3d2b2ac47f3b70fa72ca50d7abc66a0690f2f
Kristjan Oliver Kruus <kristjanokruus@gmail.com>
2018-12-01 21:53:38 +0200

    Tracker Role menüü

---
+++ Assets/_Scripts/TrackerManager_Menu_Pogo_Pin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TrackerManager_Menu_Pogo_Pin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/TrackerManager_Menu_Tracker_Roles.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TrackerManager_Menu_Tracker_Roles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
